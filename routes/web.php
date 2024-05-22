748ca9f03b9d88325a86bf2ca914e22ecedaaf2c
jcag2227 <jcag2227@gmail.com>
2024-05-22 19:21:06 +0200

    pruebas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function() {
+    return view('crud/index');
+});
+
+Route::get('/test2', function() {
+    return view('crud/link');
+})->name('crud');
